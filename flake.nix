104b3544131a827a1bd0997c9a1a291c2d3e2c33
Timm Hirsens <timmhirsens@gmail.com>
2024-01-17 20:05:33 +0100

    use homemanager module

--- flake.nix
+++ flake.nix
@@ -21,15 +21,18 @@
       nixosConfigurations = {
         nixos = lib.nixosSystem {
           inherit system;
-          modules = [ ./configuration.nix ];
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.timm = {
+                imports = [ ./home.nix ];
+              };
+            }
+          ];
         };
       };
-      homeConfigurations = {
-        timm = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./home.nix ];
-        };
-      };
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
     };
 }
