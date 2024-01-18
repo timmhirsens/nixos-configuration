8d77161a619a53c90c9fa3f5ff63090c956a7990
Timm Hirsens <timmhirsens@gmail.com>
2024-01-18 09:26:25 +0100

    zsh keybindings

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -6,6 +6,14 @@
       enableAutosuggestions = true;
       enableCompletion = true;
       initExtra = ''
+          bindkey -e
+          bindkey -M emacs "''${terminfo[khome]}" beginning-of-line
+          bindkey -M emacs "''${terminfo[kend]}"  end-of-line
+          bindkey -M emacs '^[[3;5~' kill-word
+          bindkey -M emacs '^H' backward-kill-word
+          bindkey -M emacs '^[[1;5C' forward-word
+          bindkey -M emacs '^[[1;5D' backward-word
+          bindkey '^r' history-incremental-search-backward
         	pfetch
       '';
       shellAliases = {
